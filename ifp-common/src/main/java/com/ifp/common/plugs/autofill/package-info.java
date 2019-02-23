93258cd98415e6e1cd9c30ad23c617e759c7be1c
yyk <yyk@example.com>
2019-02-23 12:48:21 +0800

    删除文件

--- ifp-common/src/main/java/com/ifp/common/plugs/autofill/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 使用AOP处理的自动填充功能，程序中某些东西，某些功能，如果想要扩展，不是那么方便，
- * 使用AOP，方便多了。
- * 使用此包需添加@EnableAspectJAutoProxy(proxyTargetClass = true)
- * @author xman 2018-12-29
- */
-package com.ifp.common.plugs.autofill;
--- ifp-common/src/main/java/com/ifp/common/plugs/function/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * 作为对jdk提供的funciton的超级补充。
- * @author xman 2019-01-03
- */
-package com.ifp.common.plugs.function;
