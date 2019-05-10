b57bd83aa5c4739f7e6b781865fa8ade8460e05f
yyk <yyk@example.com>
2019-05-10 19:01:51 +0800

    提交

---
+++ sql语句/job_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO `job_table` VALUES (1, '08f429dfc3f84546aa154b88dc726enb', 'WERCG', '车辆管理', 1, '2019-5-3', '2019-5-3', '1');
+INSERT INTO `job_table` VALUES (2, '08f429dfc3f84546aa154b88dc726ewe', 'EYJSCN', '仓库管理', 1, '2019-5-3', '2019-5-3', '1');
+INSERT INTO `job_table` VALUES (4, '6103254d516e4166be7830ab9e50447f', 'YHGSI', '配送管理', 1, '2019-5-4', '2019-5-4', '1');
